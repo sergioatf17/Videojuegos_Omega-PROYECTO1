34268bfe452d14ffb434c23b052dfbce1678023b
sergioatf17 <sergioatf17@gmail.com>
2021-07-25 17:31:56 -0500

    Pagina de videojuegos + formulario de registro

---
+++ JS/alertas.js
@@ -0,0 +1,72 @@
+Swal.fire({ 
+       title:'Bienvenidos',
+	   text: 'Selecciona tu departamento',
+	// html:
+	// icon:
+	   confirmButtonText:'Seleccionar',
+	// footer:
+	// width:
+	// padding:
+	// background:
+	// grow:
+	   backdrop: 'true',
+	// timer:
+	// timerProgressBar:
+	// toast:
+	// position:
+	   allowOutsideClick: 'true',
+	   allowEscapeKey: 'false',
+	   allowEnterKey: 'false',
+       stopKeydownPropagation: 'false',
+
+	   input: 'select',
+       inputPlaceholder: 'Departamento',
+	// inputValue:
+	 inputOptions: {
+		Amazonas:'Amazonas',
+		Antioquía:'Antioquía',
+		Arauca:'Arauca',
+		Atlántico:'Atlántico',
+		Bolívar:'Bolívar',
+		Boyacá:'Boyacá',
+		Caldas:'Caldas',
+		Caquetá:'Caquetá',
+		Casanare:'Casanare',
+		Cauca:'Cauca',
+		Cesar:'Cesar',
+	 }
+	
+	//  customClass:
+	// 	container:
+	// 	popup:
+	// 	header:
+	// 	title:
+	// 	closeButton:
+	// 	icon:
+	// 	image:
+	// 	content:
+	// 	input:
+	// 	actions:
+	// 	confirmButton:
+	// 	cancelButton:
+	// 	footer:	
+
+	// showConfirmButton:
+	// confirmButtonColor:
+	// confirmButtonAriaLabel:
+
+	// showCancelButton:
+	// cancelButtonText:
+	// cancelButtonColor:
+	// cancelButtonAriaLabel:
+	
+	// buttonsStyling:
+	// showCloseButton:
+	// closeButtonAriaLabel:
+
+
+	 //  imageUrl: 'imagenes/logo.jpg'
+	// imageWidth:
+	// imageHeight:
+	// imageAlt:
+});
